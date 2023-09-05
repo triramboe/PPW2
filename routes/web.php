60aeb11907b258070af16b29dcb7efab7bcf4835
triramboe <trnugrohoprasetyo@gmail.com>
2023-09-05 09:30:12 +0700

    update file #1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function(){
+    return view('about', [
+        "name" => "Ramboe",
+        "email" => "Ramboe@gmail.com"
+        ]); 
+});
+
