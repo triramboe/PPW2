c527a07df1bfc4498c23e0a1e94f77211e3bdbe0
triramboe <trnugrohoprasetyo@gmail.com>
2023-10-30 19:55:19 +0700

    this is my laravel_1 about auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BukuController;
 
@@ -18,15 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/about', function(){
-    return view('about', [
-        "name" => "Ramboe",
-        "email" => "Ramboe@gmail.com"
-        ]); 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/welcome', [myController::class,'hallo']);
+
+require __DIR__.'/auth.php';
 
 Route::get('/buku', [BukuController::class,'index']);
 Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
