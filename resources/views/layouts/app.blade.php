518392c5f8fa3c506d91d76f6a36c5bf6f6647ea
triramboe <trnugrohoprasetyo@gmail.com>
2023-11-21 14:05:23 +0700

    edit-dikit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="{{ asset('dist/js/lightbox-plus-jquery.min.js') }}"></script>
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -38,5 +38,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="{{ asset('dist/js/lightbox-plus-jquery.min.js') }}"></script>
     </body>
 </html>
